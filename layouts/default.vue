27c26e50c23b01975831ece2a2402cb13f63ff20
mgcrr <carriere.morgane@outlook.fr>
2021-02-07 01:12:39 +0100

    chore(project): initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="bg-primary min-h-screen text-white">
+    <Nuxt />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="uppercase">
+    <div class="text-5xl font-extrabold">
+      <h1
+        class="flex justify-center bg-clip-text text-transparent bg-gradient-to-b from-blue-600 to-blue-1100 text-6xl"
+      >
+        Enigmes
+      </h1>
+      <div class="bg-white rounded-b-full">
+        <p class="flex justify-center uppercase font-bold text-title text-2xl">
+          monde animal
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <p>allemagne</p>
+      <p>
+        <i class="fas fa-globe-africa"></i>
+      </p>
+    </div>
+
+    <div>
+      <div>
+        <div
+          class="flex justify-center items-center rounded-r-full bg-title w-40 h-10 font-bold"
+        >
+          <p class="font-extrabold text-4xl">80</p>
+          <p>énigmes</p>
+        </div>
+      </div>
+
+      <div>
+        <div
+          class="flex justify-center items-center rounded-r-full bg-player w-40 h-10 font-bold"
+        >
+          <p class="font-extrabold text-4xl">6</p>
+          <p>joueurs</p>
+        </div>
+      </div>
+
+      <div class="flex justify-end">
+        <div
+          class="flex justify-center items-center rounded-l-full bg-title w-20 h-20 font-bold"
+        >
+          <p>âge</p>
+          <p class="font-extrabold text-3xl">7+</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
